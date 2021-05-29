7c020a49a9d28ac013585dab5605d8cd8b06eb37
limen123 <limen10k@gmail.com>
2021-05-29 18:39:29 -0400

    Update 1ST STEP - CREATE BACKUPS.bat

--- 1ST STEP - CREATE BACKUPS.bat
+++ 1ST STEP - CREATE BACKUPS.bat
@@ -3,7 +3,7 @@ if exist Originals\ (GOTO :NEXT) else (mkdir Originals\ && GOTO :NEXT)
 
 :NEXT
   ECHO Searching for default game directory..
-  IF exist "%CD:~0,2%\Program Files\KH_MoM\KINGDOM HEARTS Melody of Memory_Data\StreamingAssets\Sead\music\" (ECHO Window will automatically close when backups are created && ECHO Creating backups.. && >NUL copy "%CD:~0,2%\Program Files\KH_MoM\KINGDOM HEARTS Melody of Memory_Data\StreamingAssets\Sead\music\*.*" "Originals\") else (ECHO Default directory not found! && GOTO :NEXT2)
+  IF exist "%CD:~0,2%\%ProgramFiles%\Epic Games\KH_MoM\KINGDOM HEARTS Melody of Memory_Data\StreamingAssets\Sead\music\" (ECHO Window will automatically close when backups are created && ECHO Creating backups.. && >NUL copy "%CD:~0,2%\%ProgramFiles%\Epic Games\KH_MoM\KINGDOM HEARTS Melody of Memory_Data\StreamingAssets\Sead\music\*.*" "Originals\") else (ECHO Default directory not found! && GOTO :NEXT2)
   GOTO END_CASE
 :NEXT2
   set /p id="Enter Game Directory: "
